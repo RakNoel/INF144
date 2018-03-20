14959724239b52f85395dab82792b2e4a81bfae7
Oskar Leirvåg <oskar.leir@gmail.com>
2018-03-20 20:26:10 +0100

    initial commit

---
+++ src/Compression.java
@@ -0,0 +1,12 @@
+/**
+ * TODO: Describe class
+ *
+ * @author RakNoel
+ * @version 1.0
+ * @since 20.03.18
+ */
+public class Compression {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Markov.java
@@ -0,0 +1,12 @@
+/**
+ * TODO: Describe class
+ *
+ * @author RakNoel
+ * @version 1.0
+ * @since 20.03.18
+ */
+public class Markov {
+    public static void main(String[] args) {
+
+    }
+}
