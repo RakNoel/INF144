26af2b6fbbb689e8f64a3c13881f47a09d7e8362
Oskar Leirvåg <oskar.leir@gmail.com>
2019-04-26 03:11:53 +0200

    Change  output rules

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -47,62 +47,74 @@ public class Main {
         /*
          * Generate short stories (length 30), swap boolean to stop random ending last words.
          */
+
+        /*
         for (int i = 0; i <= generations; i++) {
+
             int finalI = i;
             new Thread(() -> System.out.printf("%d degree :: %s %n", finalI, Markov.generateRandomStoryFromText(text, finalI, 100, false))).start();
         }
+         */
 
 
         /*
          * Now to the compression part, lets first compress the main short-story
          */
+        System.out.println("First off a normal LZW compression");
         System.out.println(getCompressionRate(text, LZW.compressText(text)));
 
         /*
          * Now lets do the huffman and see how stupidly much larger it gets. As this is NOT correctly in saved
          * in binary the result is even worse
          */
+        System.out.println("Now a huffman compression of an already compressed text");
         System.out.println(getCompressionRate(text, Huffman.compressText(LZW.compressText(text).replace('\u001C', ' '))));
 
         /*
-         * Now lets do this 100 times more to prove how stupid it actually is.
+         * Now lets do this a lot more times times more to prove how stupid it actually is.
          */
-        int percent = 0;
-        ArrayList<String> texter = new ArrayList<>();
-        for (int i = 0; i < iterations; i++) {
-            String x = Markov.generateRandomStoryFromText(text, generations, 10000, false);
-            texter.add(x);
 
-            if (i % (iterations / 10) == 0)
-                System.out.println((percent += 10) + "%");
+        var percent = 0;
+        System.out.println("Beginning large compression test:");
+        System.out.println("00 percent");
+
+        StringBuilder result = new StringBuilder();
+        for (int generation = 0; generation < 5; generation++) {
+            result.append(System.lineSeparator());
+            result.append(String.format("GENERATION %d: %n", generation));
+
+            ArrayList<String> texter = new ArrayList<>();
+            for (int i = 0; i < iterations; i++)
+                texter.add(Markov.generateRandomStoryFromText(text, generation, 10000, false));
+
+            //Print results
+            double sum = 0.0;
+            for (String s : texter) {
+                double comp = getCompressedBitSize(LZW.compressText(s));
+                double normal = getTheoreticalBitSize(s);
+                sum += 100 * (comp / normal);
+            }
+            result.append(String.format("LZW avg: %f %n", sum / iterations));
+
+            sum = 0.0;
+            for (String s : texter) {
+                double comp = getCompressedBitSize(Huffman.compressText(s));
+                double normal = getTheoreticalBitSize(s);
+                sum += 100 * (comp / normal);
+            }
+            result.append(String.format("Huffman avg: %f %n", sum / iterations));
+
+            sum = 0.0;
+            for (String s : texter) {
+                double comp = getCompressedBitSize(Huffman.compressText(LZW.compressText(s).replace('\u001C', ' ')));
+                double normal = getTheoreticalBitSize(s);
+                sum += 100 * (comp / normal);
+            }
+            result.append(String.format("LZW + Huffman avg: %f %n", sum / iterations));
+
+            percent += 20;
+            System.out.printf("%d percent %n", percent);
         }
-
-        System.out.println("");
-
-
-        //Print results
-        double sum = 0.0;
-        for (String s : texter) {
-            double comp = getCompressedBitSize(LZW.compressText(s));
-            double normal = getTheoreticalBitSize(s);
-            sum += 100 * (comp / normal);
-        }
-        System.out.println("LZW avg: " + sum / iterations);
-
-        sum = 0.0;
-        for (String s : texter) {
-            double comp = getCompressedBitSize(Huffman.compressText(s));
-            double normal = getTheoreticalBitSize(s);
-            sum += 100 * (comp / normal);
-        }
-        System.out.println("Huffman avg: " + sum / iterations);
-
-        sum = 0.0;
-        for (String s : texter) {
-            double comp = getCompressedBitSize(Huffman.compressText(LZW.compressText(s).replace('\u001C', ' ')));
-            double normal = getTheoreticalBitSize(s);
-            sum += 100 * (comp / normal);
-        }
-        System.out.println("LZW + Huffman avg: " + sum / iterations);
+        System.out.println(result.toString());
     }
 }
